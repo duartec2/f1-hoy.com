f054bbbb6fc7e56db05c92d3661570096100f205
Ricardo Duarte <rduarte@uxmarketing.com>
2024-08-05 23:03:21 -0600

    Google Analytics via PartyTown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,51 +1,62 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compressor from "astro-compressor";
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
-import { VitePWA } from 'vite-plugin-pwa';
+import {VitePWA} from 'vite-plugin-pwa';
+import {manifest} from './src/utils/manifest';
 
-import { manifest } from './src/utils/manifest';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4322/',
-  image: {
-    remotePatterns: [{ protocol: 'https' }],
-  },
-  markdown: {
-    drafts: true,
-    shikiConfig: {
-      theme: 'material-theme-palenight',
-      wrap: true,
+    site: 'https://f1-hoy.com/',
+    image: {
+        remotePatterns: [{
+            protocol: 'https'
+        }]
     },
-  },
-  integrations: [
-    mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: {
-        theme: 'material-theme-palenight',
-        wrap: true,
-      },
-      drafts: true,
-    }),
-    compressor({ gzip: true, brotli: true }),
-    sitemap(),
-    tailwind(),
-    robotsTxt(),
-  ],
-  vite: {
-    plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        manifest,
-        workbox: {
-          globDirectory: 'dist',
-          globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
-          navigateFallback: null,
-        },
-      }),
+    markdown: {
+        drafts: true,
+        shikiConfig: {
+            theme: 'material-theme-palenight',
+            wrap: true
+        }
+    },
+    integrations: [
+        mdx({
+            syntaxHighlight: 'shiki',
+            shikiConfig: {
+                theme: 'material-theme-palenight',
+                wrap: true
+            },
+            drafts: true
+        }),
+        compressor({
+            gzip: true,
+            brotli: true
+        }),
+        sitemap(),
+        tailwind(),
+        robotsTxt(),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"]
+            }
+        })
     ],
-  },
+    vite: {
+        plugins: [
+            VitePWA({
+                registerType: 'autoUpdate',
+                manifest,
+                workbox: {
+                    globDirectory: 'dist',
+                    globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+                    navigateFallback: null
+                }
+            })
+        ]
+    }
 });
